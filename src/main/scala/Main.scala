286471ae79977ea94efa5e3b0196c510d83a129c
a_szczepanik <a.szczepanik@adform.com>
2022-04-18 21:22:55 +0200

    Display kids

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,16 @@
+object Main extends App {
+
+  val parent = Node("AdaptiveSparkPlan")
+  val kid1 = Node("Union")
+  val kid2 = Node("HashAggregate_1")
+  val kid3 = Node("HashAggregate_2")
+
+  parent.addChildren(kid1)
+  parent.addChildren(kid2)
+  parent.addChildren(kid3)
+
+  println(parent)
+  println(parent.addChildren(kid1))
+  println(parent.addChildren(kid2))
+  println(parent.addChildren(kid3))
+}
