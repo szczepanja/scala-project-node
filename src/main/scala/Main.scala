0951c9f6b4bedb383504e31467ad3803491f9f22
a_szczepanik <a.szczepanik@adform.com>
2022-04-18 21:30:19 +0200

    Display levels of Node folders

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,15 +3,10 @@ object Main extends App {
   val parent = Node("AdaptiveSparkPlan")
   val kid1 = Node("Union")
   val kid2 = Node("HashAggregate_1")
-  val kid3 = Node("HashAggregate_2")
   val grandKids = kid1.addChildren(Node("Exchange"))
 
   parent.addChildren(kid1)
   parent.addChildren(kid2)
-  parent.addChildren(kid3)
 
-  println(parent)
-  println(parent.addChildren(kid1))
-  println(parent.addChildren(kid2))
-  println(parent.addChildren(kid3))
+  parent.display(2)
 }
--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -4,4 +4,11 @@ case class Node(children: String) {
   var kids = new ArrayBuffer[Node]
 
   def addChildren(c: Node): Unit = kids = kids :+ c
+
+  def display(n: Int): Unit = {
+    val child = children
+
+    println(" " * 4 * n + "+- " + child)
+    kids.foreach(_.display(n + 1))
+  }
 }
