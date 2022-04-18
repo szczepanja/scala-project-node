dd51a592698cc62b2098f0f3c52046242eabfc39
a_szczepanik <a.szczepanik@adform.com>
2022-04-18 21:15:27 +0200

    Create add children method

--- src/main/scala/Node.scala
+++ src/main/scala/Node.scala
@@ -1,3 +1,7 @@
-case class Node(children: String) {
+import scala.collection.mutable.ArrayBuffer
 
+case class Node(children: String) {
+  var kids = new ArrayBuffer[Node]
+
+  def addChildren(c: Node): Unit = kids = kids :+ c
 }
